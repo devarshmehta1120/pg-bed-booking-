8f1f79bb2a8fe74119bbe7f6936ad01a22e4b7fd
devarshmehta1120 <mehtadevarsh03@gmail.com>
2026-03-20 12:25:09 +0530

    solve the beds

--- frontend/src/routes/admin/beds/$roomId.jsx
+++ frontend/src/routes/admin/beds/$roomId.jsx
@@ -1,7 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import Beds from '../../../pages/admin/Beds'
 
-export const Route = createFileRoute('/admin/beds/$roomId')({
+export const Route = createFileRoute('/admin/Beds/$roomId')({
   component: Beds,
 })
 
